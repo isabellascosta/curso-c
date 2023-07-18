ab7f9715a38b17a52e69ad36284c279a5d1e8512
Isabella Costa <isa_bella411@hotmail.com>
2023-07-18 17:29:01 -0300

    Criado projeto para estudar interfaces, primeiro projeto não implementando interfaces

---
+++ Interfaces/Interfaces/Entities/Invoice.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Interfaces.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double rental, double tax)
+        {
+            BasicPayment = rental;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic Payment: "
+                + BasicPayment.ToString("f2", CultureInfo.InvariantCulture)
+            + "\nTax: "
+            + Tax.ToString("f2", CultureInfo.InvariantCulture)
+            + "\n TotalPayment: "
+            + TotalPayment.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
