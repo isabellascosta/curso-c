45c4c5aa91cb42b210419dc7635a469e58fbb2ec
Isabella Costa <isa_bella411@hotmail.com>
2023-08-04 11:45:05 -0300

    aprendendo generics, primeiro projeto sem reuso nas classes

---
+++ GenericsSetDicionary/GenericsSetDicionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Channels;
+using GenericsSetDicionary.Entities;
+namespace GenericsSetDicionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("how many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for(int i = 0; i < n;i++)
+            { 
+               int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.Write("Firt: "+printService.First());
+        }
+
+    }
+}
